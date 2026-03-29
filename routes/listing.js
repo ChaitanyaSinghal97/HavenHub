df6c67f6cdb66df0dcb5cc14be8ad4f3e3705111
ChaitanyaSinghal97 <singhalchaitanya2004@gmail.com>
2026-03-29 18:15:14 +0530

    search functionality added

--- routes/listing.js
+++ routes/listing.js
@@ -18,6 +18,23 @@ router.route("/")
 
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
+router.get("/search",wrapAsync(async(req,res)=>{
+    let{query}=req.query;
+    if(!query){
+        req.flash("error","Search query missing");
+        return res.redirect("/listings");
+    }
+    const allListings = await Listing.find({
+        $or: [
+            { title: { $regex: query, $options: "i" } },
+            { location: { $regex: query, $options: "i" } },
+            { country: { $regex: query, $options: "i" } }
+        ]
+    });
+    
+    res.render("listings/index.ejs",{allListings,query});
+}));
+
 router.route("/:id")
 .get(wrapAsync(listingController.showListing))
 .put(isLoggedIn,isOwner,upload.single("listing[image]"),validateListing,wrapAsync(listingController.updateListing))
